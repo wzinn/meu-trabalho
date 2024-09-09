770c42c298b3682d09a723d191c6cc46a27e1091
wzinn <ws91443973@gmail.com>
2024-09-09 10:50:13 -0300

    09.09

--- Meu Game 3D/Assets/New Folder/player.cs
+++ Meu Game 3D/Assets/New Folder/player.cs
@@ -1,25 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
+
+using Unity.VisualScripting;
+using UnityEditor.VersionControl;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class player : MonoBehaviour
 {
-    public int velocide = 10;
-    public Rigidbody rb;
+    public int velocidade = 10;
+    Rigidbody rb;
+    public int forcapulo = 10;
+
+    public bool noChao ;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("start");
+        Debug.Log("START");
         TryGetComponent(out rb);
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    
+    {
+        if (!noChao && collision.gameObject.tag == "noChao")
+        {
+            noChao = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("update");
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("vertical");
-        UnityEngine.Vector3 direcao = new Vector3(x, 0, y);
-        rb.AddForce(direcao * velocidade * Time.deltaTime, Forcemode.impulse)
+        Debug.Log("UPDATE");
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        UnityEngine.Vector3 direcao = new Vector3(h, 0, v);
+        rb.AddForce(direcao * (velocidade * Time.deltaTime), ForceMode.Impulse);
+        if (Input.GetKeyDown(KeyCode.Space) && noChao)
+        {
+            rb.AddForce(Vector3.up * forcapulo, ForceMode.Impulse);
+            noChao = false;
+        }
+
+        if (transform.position.y <= -10)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
     }
 }
